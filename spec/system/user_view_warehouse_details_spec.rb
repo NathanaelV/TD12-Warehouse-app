1780303d28bb2d90504aec6b71dbd3219f24c9a5
Nathanael Vieira <tmntsplinter04@gmail.com>
2024-04-07 20:24:02 -0300

    User sees warehouse datails

---
+++ spec/system/user_view_warehouse_details_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+describe 'User sees warehouse details' do
+  it 'sees additional information' do
+    # Arrange
+    Warehouse.create(name: 'Aeroporto SP', code: 'GRU', city: 'Guarulhos', area: 100_000,
+                     address: 'Avenida do Aeroporto, 1000', cep: '15000-000', 
+                     description: 'Galpão destinado para cargas internacionais')
+    
+    # Act
+    visit(root_path)
+    click_on('Aeroporto SP')
+
+    # Assert
+    expect(page).to have_content('Nome: Aeroporto SP')
+    expect(page).to have_content('Galpão GRU')
+    expect(page).to have_content('Cidade: Guarulhos')
+    expect(page).to have_content('Área: 100000 m2')
+    expect(page).to have_content('Endereço: Avenida do Aeroporto, 1000 CEP: 15000-000')
+    expect(page).to have_content('Galpão destinado para cargas internacionais')
+  end
+
+  it 'returns to home screen' do
+    # Arrange
+    Warehouse.create(name: 'Aeroporto SP', code: 'GRU', city: 'Guarulhos', area: 100_000,
+                     address: 'Avenida do Aeroporto, 1000', cep: '15000-000', 
+                     description: 'Galpão destinado para cargas internacionais')
+    
+    # Act
+    visit root_path
+    click_on 'Aeroporto SP'
+    click_on 'Menu'
+                    
+    # Assert
+    expect(current_path).to eq(root_path)
+  end
+end
