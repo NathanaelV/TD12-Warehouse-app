b34f3b10e70eb09fdd6cbd88236184a8bfae62b0
Nathanael Vieira <tmntsplinter04@gmail.com>
2024-04-12 09:32:24 -0300

    Show create erros and translates Warehouse model

--- spec/system/user_register_warehouse_spec.rb
+++ spec/system/user_register_warehouse_spec.rb
@@ -31,7 +31,7 @@ describe 'User register warehouse' do
     fill_in 'Cidade',	with: 'Rio de Janeiro'
     fill_in 'CEP',	with: '20100-000'
     fill_in 'Área',	with: '32000'
-    click_on 'Enviar'
+    click_on 'Criar Galpão'
 
     # Assert
     expect(current_path).to eq root_path
@@ -49,10 +49,12 @@ describe 'User register warehouse' do
     click_on 'Cadastrar Galpão'
     fill_in 'Nome', with: ''
     fill_in 'Descrição', with: ''
-    click_on 'Enviar'
+    click_on 'Criar Galpão'
 
     # Assert
-    expect(page).to have_content('Galpão não cadastrado')
+    expect(page).to have_content 'Galpão não cadastrado'
+    expect(page).to have_content 'Nome não pode ficar em branco'
+    expect(page).to have_content 'Descrição não pode ficar em branco'
     expect(current_path).to eq warehouses_path
   end
 end
