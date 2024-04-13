6a1dac7ff31335811b146f5279462fc28a36a564
Nathanael Vieira <tmntsplinter04@gmail.com>
2024-04-13 08:47:58 -0300

    Validates added to Supplier, all fields need to be filled in

--- spec/models/supplier_spec.rb
+++ spec/models/supplier_spec.rb
@@ -1,6 +1,69 @@
 require 'rails_helper'
 
 RSpec.describe Supplier, type: :model do
-  # Criar validadções para presença obrigatória de todos os campos
-  pending "Presença obrigatória para todos os campos"  
+  # it { is_expected.to validate_presence_of(:city) }
+  describe '#valide?' do
+    it 'false when corporate_name is empty' do
+      # Arrange
+      supplier = Supplier.new(corporate_name: '', brand_name: 'ACME', registration_number: '434472216000123',
+                              full_address: 'Av das Palmas, 100', city: 'Bauru', state: 'SP', email: 'contato@acme.com')
+
+      # Act & Assert
+      expect(supplier.valid?).to eq false
+    end
+
+    it 'false when brand_name is empty' do
+      # Arrange
+      supplier = Supplier.new(corporate_name: 'ACME LTDA', brand_name: '', registration_number: '434472216000123',
+                              full_address: 'Av das Palmas, 100', city: 'Bauru', state: 'SP', email: 'contato@acme.com')
+
+      # Act & Assert
+      expect(supplier.valid?).to eq false
+    end
+
+    it 'false when registration_number is empty' do
+      # Arrange
+      supplier = Supplier.new(corporate_name: 'ACME LTDA', brand_name: 'ACME', registration_number: '',
+                              full_address: 'Av das Palmas, 100', city: 'Bauru', state: 'SP', email: 'contato@acme.com')
+
+      # Act & Assert
+      expect(supplier.valid?).to eq false
+    end
+
+    it 'false when full_address is empty' do
+      # Arrange
+      supplier = Supplier.new(corporate_name: 'ACME LTDA', brand_name: 'ACME', registration_number: '434472216000123',
+                              full_address: '', city: 'Bauru', state: 'SP', email: 'contato@acme.com')
+
+      # Act & Assert
+      expect(supplier.valid?).to eq false
+    end
+
+    it 'false when city is empty' do
+      # Arrange
+      supplier = Supplier.new(corporate_name: 'ACME LTDA', brand_name: 'ACME', registration_number: '434472216000123',
+                              full_address: 'Av das Palmas, 100', city: '', state: 'SP', email: 'contato@acme.com')
+
+      # Act & Assert
+      expect(supplier.valid?).to eq false
+    end
+
+    it 'false when state is empty' do
+      # Arrange
+      supplier = Supplier.new(corporate_name: 'ACME LTDA', brand_name: 'ACME', registration_number: '434472216000123',
+                              full_address: 'Av das Palmas, 100', city: 'Bauru', state: '', email: 'contato@acme.com')
+
+      # Act & Assert
+      expect(supplier.valid?).to eq false
+    end
+
+    it 'false when email is empty' do
+      # Arrange
+      supplier = Supplier.new(corporate_name: 'ACME LTDA', brand_name: 'ACME', registration_number: '434472216000123',
+                              full_address: 'Av das Palmas, 100', city: 'Bauru', state: 'SP', email: '')
+
+      # Act & Assert
+      expect(supplier.valid?).to eq false
+    end
+  end
 end
