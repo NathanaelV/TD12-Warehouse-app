a5d04e6fedd694161397f60645b392342ecae0be
Nathanael Vieira <tmntsplinter04@gmail.com>
2024-04-15 13:39:31 -0300

    Rubocop

--- db/schema.rb
+++ db/schema.rb
@@ -10,56 +10,56 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_14_212028) do
-  create_table "product_models", force: :cascade do |t|
-    t.string "name"
-    t.integer "weight"
-    t.integer "height"
-    t.integer "depth"
-    t.string "sku"
-    t.integer "supplier_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer "width"
-    t.index ["supplier_id"], name: "index_product_models_on_supplier_id"
+ActiveRecord::Schema[7.1].define(version: 20_240_414_212_028) do
+  create_table 'product_models', force: :cascade do |t|
+    t.string 'name'
+    t.integer 'weight'
+    t.integer 'height'
+    t.integer 'depth'
+    t.string 'sku'
+    t.integer 'supplier_id', null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.integer 'width'
+    t.index ['supplier_id'], name: 'index_product_models_on_supplier_id'
   end
 
-  create_table "suppliers", force: :cascade do |t|
-    t.string "corporate_name"
-    t.string "brand_name"
-    t.string "registration_number"
-    t.string "full_address"
-    t.string "city"
-    t.string "state"
-    t.string "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'suppliers', force: :cascade do |t|
+    t.string 'corporate_name'
+    t.string 'brand_name'
+    t.string 'registration_number'
+    t.string 'full_address'
+    t.string 'city'
+    t.string 'state'
+    t.string 'email'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  create_table 'users', force: :cascade do |t|
+    t.string 'email', default: '', null: false
+    t.string 'encrypted_password', default: '', null: false
+    t.string 'reset_password_token'
+    t.datetime 'reset_password_sent_at'
+    t.datetime 'remember_created_at'
+    t.string 'name'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['email'], name: 'index_users_on_email', unique: true
+    t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
   end
 
-  create_table "warehouses", force: :cascade do |t|
-    t.string "name"
-    t.string "code"
-    t.string "city"
-    t.integer "area"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "address"
-    t.string "cep"
-    t.string "description"
+  create_table 'warehouses', force: :cascade do |t|
+    t.string 'name'
+    t.string 'code'
+    t.string 'city'
+    t.integer 'area'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.string 'address'
+    t.string 'cep'
+    t.string 'description'
   end
 
-  add_foreign_key "product_models", "suppliers"
+  add_foreign_key 'product_models', 'suppliers'
 end
--- spec/models/product_model_spec.rb
+++ spec/models/product_model_spec.rb
@@ -5,12 +5,12 @@ RSpec.describe ProductModel, type: :model do
     it 'name is mandatory' do
       # Arrange
       supplier = Supplier.create!(brand_name: 'Samsung', corporate_name: 'Samsung Eletronicos LTDA',
-                       registration_number: '123123123000322', full_address: 'Av Nações Unidas, 1000',
-                       city: 'São Paulo', state: 'SP', email: 'sac@samsung.com.br')
+                                  registration_number: '123123123000322', full_address: 'Av Nações Unidas, 1000',
+                                  city: 'São Paulo', state: 'SP', email: 'sac@samsung.com.br')
 
       product_models = ProductModel.new(name: '', weight: 8000, width: 80, height: 45, depth: 10, sku: 'TV32-SAMSU-XPTO90',
-                           supplier:)
-      
+                                        supplier:)
+
       # Act
       # Assert
       expect(product_models).not_to be_valid
@@ -19,12 +19,12 @@ RSpec.describe ProductModel, type: :model do
     it 'sku is mandatory' do
       # Arrange
       supplier = Supplier.create!(brand_name: 'Samsung', corporate_name: 'Samsung Eletronicos LTDA',
-                       registration_number: '123123123000322', full_address: 'Av Nações Unidas, 1000',
-                       city: 'São Paulo', state: 'SP', email: 'sac@samsung.com.br')
+                                  registration_number: '123123123000322', full_address: 'Av Nações Unidas, 1000',
+                                  city: 'São Paulo', state: 'SP', email: 'sac@samsung.com.br')
 
       product_models = ProductModel.new(name: 'TV-32', weight: 8000, width: 80, height: 45, depth: 10, sku: '',
-                           supplier:)
-      
+                                        supplier:)
+
       # Act
       # Assert
       expect(product_models).not_to be_valid
