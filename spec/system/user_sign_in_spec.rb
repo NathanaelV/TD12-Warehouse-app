e8ab1581cfcc8b17dad4ade0022f7580626fc124
Nathanael Vieira <tmntsplinter04@gmail.com>
2024-04-15 12:59:13 -0300

    User can make logout

--- spec/system/user_sign_in_spec.rb
+++ spec/system/user_sign_in_spec.rb
@@ -15,11 +15,32 @@ describe 'User autenticates' do
     end
 
     # Assert
-    expect(page).to have_link 'Sair'
+    expect(page).to have_button 'Sair'
     expect(page).not_to have_link 'Entrar'
     within('nav') do
       expect(page).to have_content 'joaozinho@email.com'
     end
     expect(page).to have_content 'Login efetuado com sucesso.'
+  end
+
+  it 'and logout' do
+    # Arrange
+    User.create!(email: 'joaozinho@email.com', password: 'password')
+
+    # Act
+    visit root_path
+    click_on 'Entrar'
+    within('form') do
+      fill_in 'E-mail', with: 'joaozinho@email.com'
+      fill_in 'Senha', with: 'password'
+      click_on 'Entrar'
+    end
+    click_on 'Sair'
+
+    # Assert
+    expect(page).to have_content 'Logout efetuado com sucesso.'
+    expect(page).to have_link 'Entrar'
+    expect(page).not_to have_button 'Sair'
+    expect(page).not_to have_content 'joaozinho@email.com'
   end
 end
