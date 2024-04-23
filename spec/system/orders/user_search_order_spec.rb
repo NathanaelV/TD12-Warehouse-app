c091e8fe5b313c56c6d8f4241d8046d73290968a
Nathanael Vieira <tmntsplinter04@gmail.com>
2024-04-23 20:51:35 -0300

    Search for multiple orders

--- spec/system/orders/user_search_order_spec.rb
+++ spec/system/orders/user_search_order_spec.rb
@@ -54,4 +54,40 @@ describe 'User search order' do
     expect(page).to have_content 'Galpão Destino: SDU - Galpão Rio'
     expect(page).to have_content 'Fornecedor: ACME LTDA'
   end
+
+  it 'finds multiple orders' do
+    # Arrange
+    user = User.create!(name: 'João', email: 'joao@email.com', password: 'password')
+    supplier = Supplier.create!(corporate_name: 'ACME LTDA', brand_name: 'ACME', registration_number: '434472216000123',
+                                full_address: 'Av das Palmas, 100', city: 'Bauru', state: 'SP',
+                                email: 'contato@acme.com')
+
+    first_warehouse = Warehouse.create!(name: 'Aeroporto SP', code: 'GRU', city: 'Guarulhos', area: 100_000,
+                                        address: 'Avenida do Aeroporto, 1000', cep: '15000-000',
+                                        description: 'Galpão destinado para cargas internacionais')
+    second_warehouse = Warehouse.create!(name: 'Galpão Rio', code: 'SDU', city: 'Rio de Janeiro', area: 60_000,
+                                         address: 'Av do Porto, 1000', cep: '20000-000', description: 'Galpão do Rio')
+
+    allow(SecureRandom).to receive(:alphanumeric).with(8).and_return('GRU12345')
+    first_order = Order.create!(user:, warehouse: first_warehouse, supplier:, estimated_delivery_date: 2.day.from_now)
+    allow(SecureRandom).to receive(:alphanumeric).with(8).and_return('GRU09876')
+    order = Order.create!(user:, warehouse: first_warehouse, supplier:, estimated_delivery_date: 2.day.from_now)
+
+    allow(SecureRandom).to receive(:alphanumeric).with(8).and_return('SDU00700')
+    order = Order.create!(user:, warehouse: second_warehouse, supplier:, estimated_delivery_date: 2.day.from_now)
+
+    # Act
+    login_as(user)
+    visit root_path
+    fill_in 'Buscar Pedido',	with: 'GRU'
+    click_on 'Buscar'
+
+    # Assert
+    expect(page).to have_content '2 pedidos encontrados'
+    expect(page).to have_content 'GRU12345'
+    expect(page).to have_content 'GRU09876'
+    expect(page).to have_content 'Galpão Destino: GRU - Aeroporto SP'
+    expect(page).not_to have_content 'SDU00700'
+    expect(page).not_to have_content 'Galpão Destino: SDU - Galpão Rio'
+  end
 end
