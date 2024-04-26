0c793664d928e53bd5496b92ce59e1afd577666d
Nathanael Vieira <tmntsplinter04@gmail.com>
2024-04-26 15:25:46 -0300

    Show products from Warehouse stock

---
+++ spec/system/product_stock/user_view_stock_from_warehouse_spec.rb
@@ -0,0 +1,39 @@
+require 'rails_helper'
+
+describe "User see the stock" do
+  it 'on warehouse details' do
+    # Arrange
+    user = User.create!(name: 'Phoenix Ikki', email: 'ikki@email.com', password: 'password')
+
+    supplier = Supplier.create!(corporate_name: 'ACME LTDA', brand_name: 'ACME', registration_number: '434472216000123',
+                                full_address: 'Av das Palmas, 100', city: 'Bauru', state: 'SP',
+                                email: 'contato@acme.com')
+
+    warehouse = Warehouse.create!(name: 'Aeroporto SP', code: 'GRU', city: 'Guarulhos', area: 100_000,
+                                  address: 'Avenida do Aeroporto, 1000', cep: '15000-000',
+                                  description: 'Galpão destinado para cargas internacionais')
+
+    order = Order.create!(user:, warehouse:, supplier:, estimated_delivery_date: 2.day.from_now)
+
+    product_a = ProductModel.create!(name: 'Produto A', weight: 1, width: 10, height: 20, depth: 30,
+                                     supplier:, sku: 'PROD-A123')
+    product_b = ProductModel.create!(name: 'Produto B', weight: 1, width: 10, height: 20, depth: 30,
+                                     supplier:, sku: 'PROD-B123')
+
+    ProductModel.create!(name: 'Produto C', weight: 1, width: 10, height: 20, depth: 30, supplier:, sku: 'PROD-C123')
+
+    3.times { StockProduct.create!(order:, warehouse:, product_model: product_a) }
+    2.times { StockProduct.create!(order:, warehouse:, product_model: product_b) }
+
+    # Act
+    login_as user
+    visit root_path
+    click_on 'Aeroporto SP'
+
+    # Assert
+    expect(page).to have_content 'Itens em Estoque'
+    expect(page).to have_content '3 x PROD-A123'
+    expect(page).to have_content '2 x PROD-B123'
+    expect(page).not_to have_content 'PROD-C123'
+  end
+end
