494388331506e10ab459672a7ab04c1268891667
Nathanael Vieira <tmntsplinter04@gmail.com>
2024-05-01 06:00:20 -0300

    Show Warehouse by API

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,25 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'home#index'
+
   resources :warehouses, only: %i[show new create edit update destroy] do
     resources :stock_product_destinations, only: [:create]
   end
+
   resources :suppliers, except: %i[destroy]
   resources :product_models, only: %i[index new create show]
+
   resources :orders, only: %i[index show new create edit update] do
     resources :order_items, only: %i[new create]
+
     get 'search', on: :collection
     post 'delivered', on: :member
     post 'canceled', on: :member
+  end
+
+  namespace :api do
+    namespace :v1 do
+      resources :warehouses, only: %i[show]
+    end
   end
 end
