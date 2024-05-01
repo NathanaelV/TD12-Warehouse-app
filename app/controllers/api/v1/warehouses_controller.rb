93bc84b46228af79d12bb3371504f40f5033d8f7
Nathanael Vieira <tmntsplinter04@gmail.com>
2024-05-01 12:42:29 -0300

    API POST Warehouse and create ApiController

--- app/controllers/api/v1/warehouses_controller.rb
+++ app/controllers/api/v1/warehouses_controller.rb
@@ -1,14 +1,27 @@
-class Api::V1::WarehousesController < ActionController::API
+class Api::V1::WarehousesController < Api::V1::ApiController
   def show
     warehouse = Warehouse.find(params[:id])
     render status: 200, json: warehouse.as_json(except: %i[created_at updated_at])
-  rescue StandardError
-    render status: 404
   end
 
   def index
     warehouses = Warehouse.all
     render 200, json: warehouses
   end
-  
+
+  def create
+    warehouse = Warehouse.new(warehouse_params)
+
+    if warehouse.save
+      render status: 201, json: warehouse
+    else
+      render status: 412, json: { errors: warehouse.errors.full_messages }
+    end
+  end
+
+  private
+
+  def warehouse_params
+    params.require(:warehouse).permit(:name, :code, :city, :area, :address, :cep, :description)
+  end
 end
